8ac7b3f729aa9ba5a5632eefdd4cdcd346e1b210
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-01 22:34:39 -0600

    Add Route to player detail page, add Link to card.

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -1,26 +1,29 @@
 import './PlayerCard.css'
 import { correctLastGameDate } from '../../helperFunctions'
+import { Link } from 'react-router-dom'
 
 
-const PlayerCard = ({ name, jerseyNum, lastGame, bat, position, headshot }) => {
+const PlayerCard = ({ name, jerseyNum, lastGame, bat, position, headshot, playerID }) => {
   console.log(lastGame)
 return (
-    <div className='player-card' tabindex="0">
-      <div className='player-headshot'>
-        <img src={headshot} className='headshot'/>
-      </div>
-      <div className='player-info'>
-        <h2 className='player-name'>
-          {name} #{jerseyNum}
-        </h2>
-        <div className='last-game'>
-          Last Game Played:<br/>{correctLastGameDate(lastGame)}
-        </div><br/>
-        <div className='positions'>
-          Bat: {bat} Position: {position}
+    <Link className='playercard' to={`/player/${playerID}`}>
+      <div className='player-card' tabindex="0">
+        <div className='player-headshot'>
+          <img src={headshot} className='headshot'/>
+        </div>
+        <div className='player-info'>
+          <h2 className='player-name'>
+            {name} #{jerseyNum}
+          </h2>
+          <div className='last-game'>
+            Last Game Played:<br/>{correctLastGameDate(lastGame)}
+          </div><br/>
+          <div className='positions'>
+            Bat: {bat} Position: {position}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
