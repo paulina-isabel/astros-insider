f2b7f9a99b69af33972515a0944746be108b2115
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-04 13:39:20 -0400

    Stash.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,13 @@
-describe('home page user flow', () => {
+describe('schedule user flow', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11', {
       statusCode: 200,
-      fixture: '.././fixtures/all-movies-test.json'
-    })
+      fixture: 'schedule.json'
+    }).as('getSchedule')
     .visit('http://localhost:3000');
   });
 
   it('should render all necessary elements for homepage view', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000')
   })
 })
