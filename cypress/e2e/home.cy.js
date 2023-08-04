36037669201da07fdbf5b5ab85cc0a7e9578dea3
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-04 16:21:36 -0400

    Pass homepage tests.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,11 +3,27 @@ describe('schedule user flow', () => {
     cy.intercept('GET', 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11', {
       statusCode: 200,
       fixture: 'schedule.json'
-    }).as('getSchedule')
-    .visit('http://localhost:3000');
+    }).as('getSchedule');
+    cy.intercept('GET', 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamRoster?teamID=11', {
+      statusCode: 200,
+      fixture: 'roster.json'
+    }).as('getRoster');
+    cy.visit('http://localhost:3000');
   });
 
   it('should render all necessary elements for homepage view', () => {
-    cy.visit('http://localhost:3000')
-  })
-})
+    cy.wait(['@getSchedule', '@getRoster']).then(([scheduleInterception, rosterInterception]) => {
+      cy.get('.nav')
+        .find('img', '.logo-link')
+        .should('have.length', 2)
+        .should('have.attr', 'src')
+        .should('include',`/`)
+      cy.get('.nav-button')
+        .should('have.length', 3)
+      cy.get('.background-image-container')
+      cy.get('.game-card')
+        .should('have.length', 1)
+      cy.get('.todays-game-header');
+    });
+  });
+});
