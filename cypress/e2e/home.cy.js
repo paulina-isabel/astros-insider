b75493f9110b2fa0ba76195452c1e3d85275dbf1
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-04 16:46:33 -0400

    Pass schedule page tests.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -17,13 +17,49 @@ describe('schedule user flow', () => {
         .find('img', '.logo-link')
         .should('have.length', 2)
         .should('have.attr', 'src')
-        .should('include',`/`)
       cy.get('.nav-button')
         .should('have.length', 3)
+      cy.get('#roster-button')
+      cy.get('#schedule-button')
+      cy.get('#favorites-button')
       cy.get('.background-image-container')
       cy.get('.game-card')
         .should('have.length', 1)
       cy.get('.todays-game-header');
     });
   });
+
+  it('should handle 500 level errors', () => {
+    cy.intercept("GET", "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11", {
+      statusCode: 500,
+      fixture: 'schedule.json'
+    })
+    cy.get('.nav')
+      .find('img', '.logo-link')
+      .should('have.length', 2)
+      .should('have.attr', 'src')
+      .should('include',`/`)
+    cy.get('.nav-button')
+      .should('have.length', 3)
+    cy.get('.background-image-container')
+    cy.get('.error-message')
+      .contains('Error: 500 -- Please refresh the page.')
+  })
+
+  it('should handle 400 level errors', () => {
+    cy.intercept("GET", "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11", {
+      statusCode: 400,
+      fixture: 'schedule.json'
+    })
+    cy.get('.nav')
+      .find('img', '.logo-link')
+      .should('have.length', 2)
+      .should('have.attr', 'src')
+      .should('include',`/`)
+    cy.get('.nav-button')
+      .should('have.length', 3)
+    cy.get('.background-image-container')
+    cy.get('.error-message')
+      .contains('Error: 400 -- Please refresh the page.')
+  })
 });
--- cypress/e2e/schedule.cy.js
+++ cypress/e2e/schedule.cy.js
@@ -1,5 +1,62 @@
-describe('template spec', () => {
+describe('should show schedule on schedule page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11', {
+      statusCode: 200,
+      fixture: 'schedule.json'
+    }).as('getSchedule');
+    cy.intercept('GET', 'https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamRoster?teamID=11', {
+      statusCode: 200,
+      fixture: 'roster.json'
+    }).as('getRoster');
+    cy.visit('http://localhost:3000');
+  });
+
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.wait(['@getSchedule', '@getRoster']).then(([scheduleInterception, rosterInterception]) => {
+      cy.get('.nav')
+        .find('img', '.logo-link')
+        .should('have.length', 2)
+        .should('have.attr', 'src')
+      cy.get('#schedule-button').click()
+      cy.get('.background-image-container')
+      cy.get('.schedule-header');
+      cy.get('.game-card')
+        .should('have.length', 12)
+        .contains('Aug 04, 2023')
+    });
+  })
+
+  it('should handle 500 level errors', () => {
+    cy.intercept("GET", "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11", {
+      statusCode: 500,
+      fixture: 'schedule.json'
+    })
+    cy.get('.nav')
+      .find('img', '.logo-link')
+      .should('have.length', 2)
+      .should('have.attr', 'src')
+      .should('include',`/`)
+    cy.get('.nav-button')
+      .should('have.length', 3)
+    cy.get('.background-image-container')
+    cy.get('.error-message')
+      .contains('Error: 500 -- Please refresh the page.')
+  })
+
+  it('should handle 400 level errors', () => {
+    cy.intercept("GET", "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamSchedule?teamID=11", {
+      statusCode: 400,
+      fixture: 'schedule.json'
+    })
+    cy.get('.nav')
+      .find('img', '.logo-link')
+      .should('have.length', 2)
+      .should('have.attr', 'src')
+      .should('include',`/`)
+    cy.get('.nav-button')
+      .should('have.length', 3)
+    cy.get('.background-image-container')
+    cy.get('.error-message')
+      .contains('Error: 400 -- Please refresh the page.')
   })
 })
