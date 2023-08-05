56fffd3fe219559fd718fb7bcff6ba66a3578823
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-08-05 15:05:20 -0400

    Improve breaking points.

--- src/components/NextGame/NextGame.js
+++ src/components/NextGame/NextGame.js
@@ -2,6 +2,7 @@ import './NextGame.css';
 import GameCard from '../GameCard/GameCard';
 import AstrosOffMessage from '../AstrosOffMessage/AstrosOffMessage';
 import todaysgame from '../../images/todaysgame.png';
+import wanthouston from '../../images/wanthouston.png';
 import { correctGamesDate } from '../../helperFunctions';
 import PropTypes from 'prop-types';
 
@@ -24,16 +25,23 @@ const NextGame = ({ scheduleData }) => {
 
   return (
     <div className='todays-game-card-container'>
-      <img src={todaysgame} className='todays-game-header' alt='todays game page header'/> 
-      {todaysCard ?
-      <GameCard 
-        gameDate={correctGamesDate(todaysCard.gameDate)}
-        gameTime={todaysCard.gameTime}
-        gameStatus={todaysCard.gameStatus}
-        away={todaysCard.away}
-        home={todaysCard.home}
-        key={todaysCard.gameID}
-      /> : <AstrosOffMessage />}
+      <p className='welcome-message'> 
+        Welcome to Astros Insider, where you can access the full 2023 season game schedule and view the current team roster. Add a player to your favorites from their details page to personalize your experience. Go 'Stros.<br></br>
+        <br></br>
+        <img src={wanthouston} className='we-want-houston' alt='we want houston'/>
+      </p>
+      <div className='homepage-content'>
+        <img src={todaysgame} className='todays-game-header' alt='todays game page header'/>
+        {todaysCard ?
+        <GameCard 
+          gameDate={correctGamesDate(todaysCard.gameDate)}
+          gameTime={todaysCard.gameTime}
+          gameStatus={todaysCard.gameStatus}
+          away={todaysCard.away}
+          home={todaysCard.home}
+          key={todaysCard.gameID}
+        /> : <AstrosOffMessage />}
+      </div>
     </div>
   );
 };
