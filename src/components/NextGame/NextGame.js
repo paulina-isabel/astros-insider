fb56d23e91dff621e76d6b5fd62df1940990f68a
Paulina Rubio-Parra <paulinaisabel@me.com>
2024-01-10 18:01:03 -0500

    Add toggle language button, base functionality.

--- src/components/NextGame/NextGame.js
+++ src/components/NextGame/NextGame.js
@@ -5,8 +5,11 @@ import todaysgame from '../../images/todaysgame.png';
 import wanthouston from '../../images/wanthouston.png';
 import { correctGamesDate } from '../../helperFunctions';
 import PropTypes from 'prop-types';
+import { useLanguage } from '../../context/LanguageContext';
 
 const NextGame = ({ scheduleData }) => {
+
+  const { language, toggleLanguage } = useLanguage();
     
   const formatDate = (date) => {
     const year = date.getFullYear().toString();
@@ -22,9 +25,12 @@ const NextGame = ({ scheduleData }) => {
     return game.gameDate === formattedDate
   });
 
-
   return (
     <div className='todays-game-card-container'>
+          <div>
+            <p>{language === 'en' ? 'Hello' : 'Hola'}</p>
+            <button onClick={toggleLanguage}>Toggle Language</button>
+          </div>
       <p className='welcome-message'> 
         Welcome to Astros Insider, where you can access the full 2023 season game schedule and view the current team roster. Updates daily. Add a player to your favorites from the player's individual detail page to personalize your experience. Go 'Stros.<br></br>
         <br></br>
--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { LanguageProvider } from './context/LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
