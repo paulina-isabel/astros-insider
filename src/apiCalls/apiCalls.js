f18ce68a13bdfc612b822d5f0710decd921fcf0f
Paulina Rubio-Parra <paulinaisabel@me.com>
2024-01-13 21:42:59 -0500

    Add language toggle to error message.

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,9 +1,9 @@
 const handleError = (response) => {
   if(!response.ok) {
-    throw new Error(`Error: ${response.status} -- Please refresh the page.`)
-  }
-  return response.json()
-}
+    throw new Error(`${response.status}`)
+  };
+  return response.json();
+};
 
 const getData = async(url) => {
   let response = await fetch(url, {
@@ -12,9 +12,9 @@ const getData = async(url) => {
       'X-RapidAPI-Key': 'bc9c0e8e5fmshb0c38a31a8522adp1e5430jsn80040d7c0eb8',
       'X-RapidAPI-Host': 'tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com'
     }
-  })
-  let data = await handleError(response)
-  return data
-}
+  });
+  let data = await handleError(response);
+  return data;
+};
 
-export default getData
+export default getData;
--- src/components/EmptyState/EmptyState.js
+++ src/components/EmptyState/EmptyState.js
@@ -1,10 +1,17 @@
 import './EmptyState.css';
 import PropTypes from 'prop-types';
+import { useLanguage } from '../../context/LanguageContext';
 
 const EmptyState = ({ errorMessage }) => {
+  const { language } = useLanguage();
+
   return (
     <div className="error-message-container">
-      <h2 className="error-message">{errorMessage.includes('Failed') ? 'Error 500 -- Please try again' : errorMessage}</h2>
+      <h2 className="error-message">
+          {language === 'en' 
+          ? `An error: ${errorMessage} occurred -- please refresh the page` 
+          : `Ocurrio un error nivel ${errorMessage} -- por favor actualiza la pagina`}
+        </h2>
     </div>
   )
 };
